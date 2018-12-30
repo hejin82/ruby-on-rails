a4cc694151e7b36c499aac3011870afd455f9a9b
hejin <hejin82@gmail.com>
2018-12-31 03:24:41 +0900

    rails generate migration add_password_digest_to_users password_digest:string

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  has_secure_password
 end
