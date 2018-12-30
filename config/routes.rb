1ec4c53c6e78d3fa90a760c270e3eb10d5c72a09
hejin <hejin82@gmail.com>
2018-12-31 02:44:44 +0900

    add gem bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   #/users/1/edit  edit
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  root "static_pages#home"
 end
