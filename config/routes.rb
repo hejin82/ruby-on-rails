3bd11f3a0e016612751095f15a8194a061ac51fc
hejin <hejin82@gmail.com>
2018-12-31 01:25:05 +0900

    add routes comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #/users         index
+  #/users/1       show
+  #/users/new     new
+  #/users/1/edit  edit
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#hello"
